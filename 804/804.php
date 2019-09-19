88c8aaf41b2e372abf8e51db23f9843ed6220db0
lethanh99 <lethanh131199@gmail.com>
2019-09-19 18:44:27 +0700

    moi

--- 804/804.php
+++ 804/804.php
@@ -18,12 +18,17 @@ $less->compileFile('less/804.less', 'css/804.css');
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>804</title>
+        <title>Module - 804</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo $url_path ?>/css/804.css" rel="stylesheet" type="text/css" />
-        
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet"> 
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
